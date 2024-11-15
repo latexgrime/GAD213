c2bcf3afa7d151e74780c516950238622b1cb392
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-11-15 17:17:32 -0300

    Corrected the rotation bug and eliminated outdated code.
    THIS SHOULDN'T HAVE TAKEN THIS LONG. IM SAD

--- Assets/_Leonardo Estigarribia/_Scripts/InventorySystem/InventoryItem.cs
+++ Assets/_Leonardo Estigarribia/_Scripts/InventorySystem/InventoryItem.cs
@@ -17,39 +17,8 @@ namespace LeonardoEstigarribia.InventorySystem.inventoryItem
     /// </summary>
     public class InventoryItem : MonoBehaviour
     {
-        public ItemData itemData;
-
         public bool isRotated;
 
-        // Sets the data of the item scriptable object to the empty prefab.
-        public void SetNormalShapedItemData(ItemData itemData)
-        {
-            // Set the reference of the passed itemData to be the itemData referenced in this script (which should be in the itemPrefabGameObject).
-            this.itemData = itemData;
-
-            // Set the image.
-            GetComponent<Image>().sprite = itemData.itemIcon;
-
-            // Set the size of the image by using the rect sizeDelta.
-            var size = new Vector2();
-            size.x = itemData.width * ItemGrid.tileSizeWidth;
-            size.y = itemData.height * ItemGrid.tileSizeHeight;
-            GetComponent<RectTransform>().sizeDelta = size;
-        }
-
-        public void RotateItemNormalShapedItem()
-        {
-            isRotated = !isRotated;
-
-            var itemRect = GetComponent<RectTransform>();
-
-            // This checks if the isRotated bool is true.
-            // If TRUE, the angle value will be 90. 
-            // If FALSE, the angle value will be 0.
-            itemRect.rotation = Quaternion.Euler(0, 0, isRotated ? 90f : 0);
-        }
-
-        #region Complex-Shaped Items
 
         public ItemDataComplexShaped itemDataComplexShaped;
         public int complexHeight;
@@ -57,17 +26,17 @@ namespace LeonardoEstigarribia.InventorySystem.inventoryItem
         public int onGridPositionX;
         public int onGridPositionY;
 
-        public bool[,] itemShape
-        {
-            get => itemDataComplexShaped.shape; // 2D Array of bools that holds the shape of the object.
-            set => itemDataComplexShaped.shape = value;
-        }
+        public bool[,] itemShape;
 
 
         public void SetComplexItem(ItemDataComplexShaped _itemDataComplexShaped)
         {
             itemDataComplexShaped = _itemDataComplexShaped;
-
+            
+            // Set the shape and set it to this specific object.
+            itemDataComplexShaped.SetShape();
+            itemShape = itemDataComplexShaped.shape;
+            
             complexHeight = _itemDataComplexShaped.SetShape().GetLength(0);
             complexWidth = _itemDataComplexShaped.SetShape().GetLength(1);
 
@@ -146,11 +115,8 @@ namespace LeonardoEstigarribia.InventorySystem.inventoryItem
             }
 
             // Update the shape 2D array to be the rotated one.
-            itemDataComplexShaped.shape = rotatedShape;
+            itemShape = rotatedShape;
             (complexWidth, complexHeight) = (complexHeight, complexWidth);
         }
-
-        
-        #endregion
     }
 }
