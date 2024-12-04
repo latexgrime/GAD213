548297a5631870a96382eddf7071f2636f827c9c
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-12-04 17:44:13 +0400

    Simple enemy AI done.

--- Assets/_Leonardo Estigarribia/_Scripts/Enemy/ChaseState.cs
+++ Assets/_Leonardo Estigarribia/_Scripts/Enemy/ChaseState.cs
@@ -4,15 +4,19 @@ namespace _Leonardo_Estigarribia._Scripts.Enemy
 {
     public class ChaseState : State
     {
+        private IdleState idleState;
         private AttackState attackState;
         private bool isInAttackRange;
-    
+        private bool isInChaseRadius;
+
+        [SerializeField] private float chaseRadius = 10f;
         [SerializeField] private float moveSpeed = 5f;
         [SerializeField] private float attackRange = 2f;
         private Transform player;
     
         private void Start()
         {
+            idleState = GetComponent<IdleState>();
             attackState = GetComponent<AttackState>();
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
@@ -20,14 +24,22 @@ namespace _Leonardo_Estigarribia._Scripts.Enemy
         public override State RunCurrentState()
         {
             if (player == null) return this;
-            
+            if (!CheckForChaseRadius())
+            {
+                return idleState;
+            }
+            CheckForAttackRange();
             MoveTowardsPlayer();
             LookAtPlayer();
-            CheckForAttackRange();
-        
+            
             if (isInAttackRange)
                 return attackState;
             return this;
+        }
+
+        private bool CheckForChaseRadius()
+        {
+            return isInChaseRadius = Vector3.Distance(transform.root.position, player.position) <= chaseRadius;
         }
 
         private void CheckForAttackRange()
