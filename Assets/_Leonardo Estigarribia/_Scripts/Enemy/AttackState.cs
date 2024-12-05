c1303c7978fdd47ee139f38afbec5915c311fb07
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-12-05 17:40:09 +0400

    Reformatted using Rider.

--- Assets/_Leonardo Estigarribia/_Scripts/Enemy/AttackState.cs
+++ Assets/_Leonardo Estigarribia/_Scripts/Enemy/AttackState.cs
@@ -10,50 +10,77 @@ namespace _Leonardo_Estigarribia._Scripts.Enemy
         private StateManager stateManager;
         private ChaseState chaseState;
         private Transform player;
+        private EntityStats playerStats;
 
         [SerializeField] private float attackingEventDelay;
         [SerializeField] private UnityEvent attackingEvent;
         
+        [SerializeField] private float attackRange = 2f;
         [SerializeField] private float attackCooldown = 1f;
-        [SerializeField] private float attackDamage = 10f;
+        [SerializeField] private int attackDamage = 10;
         private float nextAttackTime;
+        private bool isPlayerNull;
+        
+        [SerializeField] private float attackRadius = 1f;
+        [SerializeField] private Vector3 attackOffset = new Vector3(0, 1f, 1f);
 
         private void Start()
         {
             player = GameObject.FindWithTag("Player").transform;
+            isPlayerNull = player == null;
             stateManager = GetComponent<StateManager>();
             chaseState = GetComponent<ChaseState>();
         }
         
-        [SerializeField] private float attackRange = 2f;
         public override State RunCurrentState()
         {
-            if (player == null) return this;
+            if (isPlayerNull) return this;
 
             float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            
             if (distanceToPlayer > attackRange)
             {
-                stateManager._animator.SetFloat("Horizontal", 0f); // Reset animation
+                stateManager._animator.SetFloat("Horizontal", 0f);
                 return chaseState;
             }
 
             if (Time.time >= nextAttackTime)
             {
-                StartCoroutine(PerformAttack());
+                StartCoroutine(Attack());
                 nextAttackTime = Time.time + attackCooldown;
             }
     
-            return this; // Stay in attack state while in range
+            return this;
         }
     
-        private IEnumerator PerformAttack()
+        private IEnumerator Attack()
         {
-            Debug.Log($"Attacking for {attackDamage} damage!");
             stateManager._animator.SetTrigger("Attack");
 
             yield return new WaitForSeconds(attackingEventDelay);
             
+            // Create attack sphere in front of enemy
+            Vector3 attackPos = transform.position + transform.forward * attackOffset.z + transform.up * attackOffset.y;
+            Collider[] hitColliders = Physics.OverlapSphere(attackPos, attackRadius, LayerMask.GetMask("Player"));
+
+            // Check if player was hit
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.TryGetComponent<EntityStats>(out var entityStats))
+                {
+                    entityStats.TakeDamage(attackDamage);
+                    break;
+                }
+            }
+       
             attackingEvent.Invoke();
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            Vector3 attackPos = transform.position + transform.forward * attackOffset.z + transform.up * attackOffset.y;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPos, attackRadius);
         }
     }
 }
---
+++ Assets/_Leonardo Estigarribia/_Scripts/Enemy/DieState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace _Leonardo_Estigarribia._Scripts.Enemy
+{
+    public class DieState : State
+    {
+        private StateManager _stateManager;
+        public override State RunCurrentState()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
