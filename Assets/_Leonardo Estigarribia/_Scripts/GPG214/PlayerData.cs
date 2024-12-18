c4ff40233ae25ef63c73dedc7a6609601e0804c5
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-12-18 19:34:08 +0400

    Saving and loading data locally now works.
    Bugs:
    - The player sometimes doesn't get teleported to the position they were in.

--- Assets/_Leonardo Estigarribia/_Scripts/GPG214/PlayerData.cs
+++ Assets/_Leonardo Estigarribia/_Scripts/GPG214/PlayerData.cs
@@ -7,11 +7,22 @@ namespace _Leonardo_Estigarribia._Scripts.GPG214
     public class PlayerData : MonoBehaviour
     {
         public GameObject player;
+        public EntityStats playerEntityStats;
         
         public Image playerIcon;
         public Vector3 playerPosition;
         public string playerName;
         public string playerId;
+
+        private void Start()
+        {
+            playerEntityStats = player.GetComponent<EntityStats>();
+        }
+
+        private void Update()
+        {
+            playerPosition = player.transform.position;
+        }
 
         public void SetPlayerPosition(float x, float y, float z)
         {
