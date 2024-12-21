bc739ca12b0d54da19117a7b21e2a2a15ec35bc2
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-12-21 21:47:12 +0400

    Progressing on the creation of the uploader/downloader of asset bundles.

--- Assets/_Leonardo Estigarribia/_Scripts/GPG214/GoogleDriveAssetBundleManager.cs
+++ Assets/_Leonardo Estigarribia/_Scripts/GPG214/GoogleDriveAssetBundleManager.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityGoogleDrive;
 
 namespace _Leonardo_Estigarribia._Scripts.GPG214
 {
@@ -42,6 +48,80 @@ namespace _Leonardo_Estigarribia._Scripts.GPG214
             {
                 Destroy(gameObject);
             }
+        }
+
+        public async Task<byte[]> DownloadAssetBundle(string bundleName)
+        {
+            var taskCompletionSource = new TaskCompletionSource<byte[]>();
+
+            var bundleInfo = savedBundles.Find(b => b.BundleName == bundleName);
+            if (bundleInfo == null)
+            {
+                Debug.LogError($"No asset bundle found with name: {bundleName}");
+                taskCompletionSource.SetResult(null);
+                return await taskCompletionSource.Task;
+            }
+
+            StartCoroutine(DownloadBundleCoroutine(bundleInfo.BundleId, downloadedData =>
+            {
+                taskCompletionSource.SetResult(downloadedData);
+            }));
+            return await taskCompletionSource.Task;
+        }
+
+        private IEnumerator DownloadBundleCoroutine(string bundleId, Action<byte[]> onComplete)
+        {
+            Debug.Log($"Downloaded Asset Bundle with ID: {bundleId}");
+
+            var request = GoogleDriveFiles.Download(bundleId);
+            yield return request.Send();
+
+            if (request.IsError)
+            {
+                Debug.LogError($"Could not complete asset bundle download. Exception: {request.Error}");
+                onComplete?.Invoke(null);
+            }
+
+            Debug.Log($"Successfully downloaded asset bundle");
+            onComplete?.Invoke(request.ResponseData.Content);
+        }
+
+        public async Task<bool> UploadAssetBundle(string bundlePath, string bundleName)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            byte[] bundleData = File.ReadAllBytes(bundlePath);
+            StartCoroutine(UploadBundleCoroutine(bundleData, bundleName, success =>
+            {
+                taskCompletionSource.SetResult(success);
+            }));
+            return await taskCompletionSource.Task;
+        }
+
+        private IEnumerator UploadBundleCoroutine(byte[] bundleData, string bundleName, Action<bool> onComplete)
+        {
+            Debug.Log($"Uploading Asset Bundle: {bundleName}");
+            var file = new UnityGoogleDrive.Data.File()
+            {
+                Name = bundleName,
+                Content = bundleData
+            };
+
+            var request = GoogleDriveFiles.Create(file);
+            request.Fields = new List<string> { "id" };
+            
+            yield return request.Send();
+            
+            if (request.IsError)
+            {
+                Debug.LogError($"Could not upload Asset Bundle. Exception: {request.Error}");
+                onComplete?.Invoke(false);
+            }
+
+            var bundleId = request.ResponseData.Id;
+            
+            Debug.Log($"Successfully uploaded asset bundle. {bundleId}");
+            onComplete?.Invoke(true);
         }
 
         private void LoadBundleList()
